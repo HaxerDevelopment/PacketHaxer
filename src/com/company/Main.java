a8dc3eed066d22515376cc8cef4c432265df8a6f
Ladiarus <yarlad03@gmail.com>
2020-10-03 19:23:55 +0300

    Default template

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,31 @@
 package com.company;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseListener;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws Exception{
+        Frame frame = new Frame("Haxer");
+        frame.setVisible(true);
+        frame.setSize(1200, 720);
+        Button b1 = new Button("12");
+        b1.addActionListener(e -> System.out.println("SSSSSSSSSSSSSSSSSSSSSSSSSSSAAAAAAAAAAAAAAAAAAASSSSSSSSSSSSSSSSSS"));
+        frame.add(b1);
+        frame.add(b1);
+        System.out.println("Enter packet to hax\n");
+        //String s = reader.readLine();
+        //int a = Integer.parseInt(s);
         System.out.println("Let's hack some packets");
-
+        Thread.sleep(1000);
+        for (int i = 0; i <= 100; i++) {
+            System.out.println(i+"%");
+            Thread.sleep((100));
+        }
+        System.out.println("Successfully hacked");
     }
 }
